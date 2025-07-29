f1b453c6ab472998aca0ea88e2206baa0df94a73
Diego Fernandes <9712di@gmail.com>
2025-07-29 15:55:52 -0300

    criando e conectando a ConsumptionActivity e validando campo vazio

--- app/src/main/java/com/example/fuelcalculator/ConsumptionActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/ConsumptionActivity.kt
@@ -1,30 +1,20 @@
 package com.example.fuelcalculator
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ConsumptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_consumption)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val iniciar = findViewById<Button>(R.id.btn_iniciar)
-
-        iniciar.setOnClickListener{
-            val intent = Intent(this, PriceActivity::class.java)
-            startActivity(intent)
         }
     }
 }
