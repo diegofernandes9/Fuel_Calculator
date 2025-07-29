d93a1f6260cfc729a5914bb8b0328c75a6e5a016
Diego Fernandes <9712di@gmail.com>
2025-07-29 12:57:10 -0300

    criando a segunda tela e criando conecção entre elas

--- app/src/main/java/com/example/fuelcalculator/PriceActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/PriceActivity.kt
@@ -1,32 +1,22 @@
 package com.example.fuelcalculator
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PriceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_price)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val iniciar = findViewById<Button>(R.id.btn_iniciar)
-        var iniciarstr = iniciar.text.toString()
-
-        iniciar.setOnClickListener{
-            val intent = Intent(this, PriceActivity::class.java)
-            intent.putExtra("123" , iniciarstr)
-            startActivity(intent)
-        }
+        val iniciar = intent.getStringExtra("123")
     }
 }
